35788d3f77745b46e0806172bed3612567fa60c4
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-07 23:12:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+	let newStr = str.split(' ')join('');
 
+	let left = 0;
+	let right = newStr.length-1;
+
+	while(left < right){
+		if(newStr[left] != newStr[right]){
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
 }
 module.exports = palindrome
